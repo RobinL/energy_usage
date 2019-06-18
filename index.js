f2781cb8bec4c57da64eaacdda0eda091c54360d
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-06-18 10:56:35 +0100

    resolving issues

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-export { get_daily_litres } from './src/shower.js';
+function get_daily_litres(num_showers, shower_minutes, shower_flow) {
+    return num_showers * shower_minutes * shower_flow * 60 / 1000;
+}
+
+module.exports  = {
+    'get_daily_litres': get_daily_litres
+};
--- src/shower.js
+++
@@ -1,3 +0,0 @@
-function get_daily_litres(num_showers, shower_minutes, shower_flow) {
-    return num_showers * shower_minutes * shower_flow * 60 / 1000;
-}
