d2ec6fd87f57479d5c5594005ca3340021f938b9
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-08-18 09:45:08 +0100

    works, but need to avoid circular conversions

--- src/constants.js
+++ src/constants.js
@@ -78,34 +78,62 @@ Object.keys(constants).forEach(from_key => {
 constants = Object.assign({}, constants, inverse_constants);
 
 
-// There must be a recursive form for this algorithm
-function find_implicit_conversions() {
-    // Generate conversions that 'pass through' an intermediate conversion e.g. hours to minutes
-    Object.keys(constants).forEach(from_key => {
-        // from key is e.g. days
-        let this_constant = constants[from_key]
-        Object.keys(this_constant).forEach(to_key => {
-            //to_key is e.g. hours
+function follow_path(start_key, from_key, to_key, multiplier, depth) {
+    // Multiplier is the constant multiplier computed so far
+    // Want from_key to stay constant as we traverse, passing through current conversion
+    // Intermediate keys will be picked up on a different start_key iteration
 
-            if (to_key in constants) {
-                let final_keys = constants[to_key]
-                // intermediate_constants is {"minutes": 60, "seconds": 60}
-                Object.keys(final_keys).forEach(final_key => {
-                    // final_key is minutes
-                    if (final_key != from_key) {
-                        constants[from_key][final_key] = constants[from_key][to_key] * constants[to_key][final_key]
-                    }
-                })
+    // TODO: I don't think depth should be needed
+    // Instead we just need to check we're not 'self converting' e.g. joules to joules
+    depth += 1
+
+    if (depth > 10) {return}
+
+
+
+    // console.log(`----following from start ${start_key} from key ${from_key} to  ${to_key}`)
+
+    // debugger;
+
+    if (to_key in constants) {// then to_key is also a from_key and chaining is possible
+
+        // Update multiplier with current conversion to pass into follow_path
+        multiplier = multiplier * constants[from_key][to_key]
+
+        // for chain_key in to_key
+        let conversions = constants[to_key]
+
+        // console.log(`--chaining possible through from_key ${from_key} ${to_key} ${JSON.stringify(conversions)}`)
+
+        // How to prevent 'circular' conversions e.g. litres_petrol -> joules -> litres_jet_fuel -> joules
+
+        Object.keys(conversions).forEach(inner_key => {
+
+            if (start_key != inner_key) {
+                console.log(`ADDING CHAIN ${start_key} to ${inner_key} with ${multiplier * constants[to_key][inner_key]}`)
+
+                new_constants[start_key] = new_constants[start_key] || {}
+                new_constants[start_key][inner_key] = multiplier * constants[to_key][inner_key]
+
+                // Making this conditional on this path not already existing
+                follow_path(start_key, to_key, inner_key, multiplier, depth)
             }
-        })
-    });
+        });
+    }
 }
 
-find_implicit_conversions()
-find_implicit_conversions()
-find_implicit_conversions()
-find_implicit_conversions()
+let new_constants = {}
 
+Object.keys(constants).forEach(from_key => {
+    // console.log(`======constants key ${from_key}`)
+    let from_val = constants[from_key]
+    Object.keys(from_val).forEach(to_key => {
+        // console.log(`===constants to key ${to_key}`)
+        follow_path(from_key, from_key, to_key, 1, 1)
+    })
+});
+
+constants = Object.assign({}, constants, new_constants);
 
 
 
