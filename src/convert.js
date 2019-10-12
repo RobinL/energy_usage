2a69b87903ec7fbafb2155d2aa07d29af57b59cf
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-10-12 12:36:26 +0100

    added source and description

--- src/convert.js
+++ src/convert.js
@@ -1,31 +1,47 @@
 
 let constants = {}
 
-// Where URL params are specified, the source is a google serach
+const template_dict = {"conversion": null,
+                "sources": [],
+                "desc": null}
+
+function shallow_copy(obj) {
+    return Object.assign({}, obj);
+}
+
+// Where URL params are specified, the sources is a google serach
 // e.g. q=one+km+in+miles is www.google.com/search?q=one+km+in+miles
 
 // Distances
 constants["kilometers"] = {
-    "meters": 1000,
-    "miles": 0.621371  //q=one+km+in+miles
+    "meters": {"conversion":1000, "sources": [], "desc": null},
+    "miles": {"conversion": 0.621371, "sources": ["www.google.com/search?q=one+km+in+miles"], "desc": null}
 }
 
-// Volumes
+// // Volumes
 constants["litres"] = {
-    "imperial_gallons": 0.219969204701183, //?q=one+litre+in+imperial+gallons
-    "us_gallons": 0.264172, //q=one+litre+in+us+gallons
-    "oil_barrels": 0.00628981 //q=one+litre+in+oil+barrel
+    "imperial_gallons": { "conversion": 0.219969204701183, "sources": ["www.google.com/search?q=one+litre+in+imperial+gallons"], "desc":null},
+    "us_gallons": { "conversion": 0.264172, "sources": ["www.google.com/search?q=one+litre+in+us+gallons"], "desc":null},
+    "oil_barrels": { "conversion": 0.00628981, "sources": ["www.google.com/search?q=one+litre+in+oil+barrel"], "desc":null} //q=one+litre+in+oil+barrel
 }
 
 // Units of energy
 constants["joules"] = {
-    ["kilocalories"]: 1 / 4184, //q=one+joule+in+kilocalories
-    ["kilowatt_hours"]: 1 / (60 * 60 * 1000)
+    "kilocalories": { "conversion": 1 / 4184, "sources": ["www.google.com/search?q=one+joule+in+kilocalories"], "desc":null},
+    "kilowatt_hours": {"conversion" :1 / (60 * 60 * 1000), "sources":[], "desc":null}
 }
 
-// Energy densities
+constants["kilowatt_hours"] = {
+    "megawatt_hours": {"conversion":1/1000, "sources":[], "desc": null}
+}
+
+constants["megawatt_hours"] = {
+    "gigawatt_hours": {"conversion":1/1000, "sources":[], "desc": null}
+}
+
+// // Energy densities
 constants["litres_petrol"] = {
-    "joules": 34.2e6, //https://en.wikipedia.org/wiki/Energy_density,
+    "kilowatt_hours": { "conversion": 0.23324, "sources": ["https://en.wikipedia.org/wiki/Energy_density", "Energy Saving Trust Conversion Factors 2016"], "desc": null}, //
     "imperial_gallons_petrol": constants["litres"]["imperial_gallons"]
 }
 
@@ -36,34 +52,45 @@ constants["litres_jet_fuel"] = {
     "kg_co2_jet_fuel": 3.15 //https://www.carbonindependent.org/22.html   'which gives CO2 emissions from a Boeing 737-400 of 115 g per passenger km.'
 }
 
-constants["imperial_gas_units"] = {
-    "kilowatt_hours": (39.5 / 3.6) * 2.83 // https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation
-}
+// constants["imperial_gas_units"] = {
+//     "kilowatt_hours": (39.5 / 3.6) * 2.83 // https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation
+// }
 
-constants["metric_gas_units"] = {
-    "kilowatt_hours": (39.5 / 3.6)// https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation
-}
+// constants["metric_gas_units"] = {
+//     "kilowatt_hours": (39.5 / 3.6)// https://www.gov.uk/guidance/gas-meter-readings-and-bill-calculation
+// }
 
-// Time constants
-constants["years"] = {
-    "days": 365.25,
-    "months": 12
-}
+// // Time constants
+// constants["years"] = {
+//     "days": 365.25,
+//     "months": 12
+// }
 
-constants["weeks"] = {
-    "days": 7
-}
+// constants["weeks"] = {
+//     "days": 7
+// }
 
 constants["days"] = {
-    "hours": 24
+    "hours": {
+        "conversion": 24,
+        "sources": ["google_days_hours"],
+        "desc": null
+    }
 }
 
 constants["hours"] = {
-    "minutes": 60
+    "minutes": {
+        "conversion": 60,
+        "sources": ["google_hours_minutes"],
+        "desc": null
+    }
 }
 
 constants["minutes"] = {
-    "seconds": 60
+    "seconds": {
+        "conversion": 60,
+        "sources": ["google_minutes_seconds"],
+        "desc": null}
 }
 
 // Also include the inverse of all conversions
@@ -71,7 +98,11 @@ Object.keys(constants).forEach(from_key => {
     let this_constant = constants[from_key]
     Object.keys(this_constant).forEach(to_key => {
         constants[to_key] = constants[to_key] || {}
-        constants[to_key][from_key] = 1 / constants[from_key][to_key]
+        constants[to_key][from_key] = shallow_copy(template_dict)
+        constants[to_key][from_key]["conversion"] = 1 / constants[from_key][to_key]["conversion"]
+        constants[to_key][from_key]["sources"] = [...constants[from_key][to_key]["sources"]]
+        constants[to_key][from_key]["desc"] = constants[from_key][to_key]["desc"]
+
     })
 });
 
@@ -86,10 +117,9 @@ function follow_path(start_key, from_key, to_key, multiplier, path) {
     if (to_key in constants) {// then to_key is also a from_key and chaining is possible
 
         // Update multiplier with current conversion
-        multiplier = multiplier * constants[from_key][to_key]
+        multiplier = multiplier * constants[from_key][to_key]["conversion"]
 
         let conversions = constants[to_key]
-
 
         Object.keys(conversions).forEach(inner_key => {
 
@@ -97,7 +127,11 @@ function follow_path(start_key, from_key, to_key, multiplier, path) {
             if (!(path.includes(inner_key))) {
 
                 if (!(inner_key in constants[start_key])) {  //If there isn't already a entry for this inner_key
-                    constants[start_key][inner_key] = multiplier * constants[to_key][inner_key]
+                    constants[start_key][inner_key] = shallow_copy(template_dict)
+                    constants[start_key][inner_key]["conversion"] = multiplier * constants[to_key][inner_key]["conversion"]
+                    constants[start_key][inner_key]["sources"] = [...constants[to_key][inner_key]["sources"], ...constants[from_key][to_key]["sources"]]
+                    constants[start_key][inner_key]["desc"] = constants[to_key][inner_key]["desc"]
+
                     follow_path(start_key, to_key, inner_key, multiplier, [...path])
                 }
             }
@@ -115,6 +149,8 @@ Object.keys(constants).forEach(from_key => {
 
 function expand_abbreviations(unit) {
     let lookup = {
+        "gwh": "gigawatt_hours",
+        "mwh": "megawatt_hours",
         "kwh": "kilowatt_hours",
         "kcal": "kilocalories",
         "km": "kilometers",
@@ -131,7 +167,7 @@ function convert_units(from, to) {
     from = expand_abbreviations(from)
     to = expand_abbreviations(to)
 
-    if (from == to) {return 1}
+    if (from == to) { return 1 }
 
     if (!(from in constants)) {
         let units_available = Object.keys(constants).join(", ")
